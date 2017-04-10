44c2ef812612a432a52f7d30b95826adbf8480f2
Derian Andersen <derian.andersen@dowjones.com>
2017-04-10 15:41:51 -0500

    fix: remove test code from index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const listener = require('./listener');
 
-// module.exports = listener;
-
-const onMessageCallback = (msg, topic) => {
-  console.log(`Received '${topic}' message: ${JSON.stringify(msg.data)}\r\n`);
-};
-
-listener.listen(onMessageCallback);
+module.exports = listener;
