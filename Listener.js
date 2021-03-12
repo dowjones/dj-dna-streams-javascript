ee4bcd257b95607cc02909d497d4c2ebfcfdee75
Zachary Kagan <zakagan@gmail.com>
2021-03-12 09:06:40 -0600

    update ack order

--- Listener.js
+++ Listener.js
@@ -68,8 +68,8 @@ class Listener {
     console.log(`Listening to subscription: ${subscriptionFullName}`);
 
     const onMessage = (msg) => {
+      onMessageCallback(msg);
       msg.ack();
-      return onMessageCallback(msg);
     };
 
     const pubsubSubscription = this.pubsubClient.subscription(subscriptionFullName);
